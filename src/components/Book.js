a2aa89992718b7a1ab5064873a4c9031b56b348e
unknown <pedro.henrickcs@gmail.com>
2019-03-25 23:49:19 -0300

    iterator books

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,18 +4,18 @@ class Book extends Component {
 
     
     render() {
-        // const book = title;
-
-        const { bookName } = this.props
+        const book = this.props.book
+        console.log('book', book);
+        
         return (
-            <ol>
-                {bookName.map(bookName => (
-                    <li>
-                        <div className="book">
-                            <div key={bookName.name}>{bookName.name}</div>
+            <ol className="books-grid">
+                <li key={book}>
+                    <div className="book">
+                        <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                         </div>
-                    </li>
-                ))}
+                    </div>
+                </li>
             </ol>
         )
     }
